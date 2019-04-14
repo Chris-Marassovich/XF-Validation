af1a6012303d28549be6b20a7df77bd0edef5baf
Chris Marassovich <chris.marassovich@outlook.com>
2019-04-14 15:37:28 +1000

    Solution created.

---
+++ XFValidationApp1/XFValidationApp1/XFValidationApp1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFValidationApp1.Models;
+
+namespace XFValidationApp1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
